d1b3406879d5c9c1afd000108fd2ec11ef61ca64
Chris Varenhorst <varenc@gmail.com>
2018-12-15 15:49:54 -0800

    switch to using watchdog instead of polling clipboard...and start handler in different thread to maybe prevent watchdog freezing

--- dropbox-url-swap.py
+++ dropbox-url-swap.py
@@ -3,6 +3,14 @@ import time
 import sys
 #sys.stdout = open("/tmp/dropbox-link-service.txt", "w+")
 
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+from watchdog.events import PatternMatchingEventHandler  
+
+import logging
+import threading
+
+
 
 def write_to_clipboard(output):
     process = subprocess.Popen(
@@ -14,15 +22,49 @@ def read_from_clipboard():
     return subprocess.check_output(
         'pbpaste', env={'LANG': 'en_US.UTF-8'}).decode('utf-8')
 
-old_clip = read_from_clipboard()
-while True:
-    clip = read_from_clipboard()
-    if old_clip != clip:
-        #print('clipboard changed!')
+
+class MyHandler(PatternMatchingEventHandler):
+    def on_created(self, event):
+        print('handling new event!!!!!', flush=True)
+        ethread = threading.Thread(target=event_handler, args=(event,))
+        ethread.start()
+        #event_handler(event)
+
+def event_handler(event):
+    if any(x in event.src_path.lower() for x in ['screen shot', 'screenshot']):
+        print('SCREEN SHOT MATCH!!!!!!!!!', event.src_path, flush=True)
+    else:
+        print('faaillll', event.src_path, flush=True)
+        return
+
+    for i in range(15): # give dropbox 15 * 0.3 seconds
+        clip = read_from_clipboard()
         if '?dl=0' in clip:
             print('clipboard has a dropbox url in it! swapping...', flush=True)
             clip = clip.replace('?dl=0', '?raw=1')
             write_to_clipboard(clip)
-    old_clip = clip
-    time.sleep(2)
+            return
+        else:
+            print('failed on', clip, flush=True)
+        time.sleep(0.3)
+    return
     
+
+
+print('starting up!!!!!', flush=True)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+path = "/Users/chris/Desktop"
+observer = Observer()
+#event_handler = LoggingEventHandler()
+#event_handler = MyHandler()
+observer.schedule(MyHandler(), path, recursive=False)
+observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+
