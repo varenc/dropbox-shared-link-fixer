f3c7b4690e55bcbcfb803b1de9c72214d1d4a136
Chris Varenhorst <varenc@gmail.com>
2020-02-19 16:25:06 -0800

    crappy fsevents based url swapper.  actually this is way less efficient

--- dropbox-url-swap.py
+++ dropbox-url-swap.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import sys
+import os
 #sys.stdout = open("/tmp/dropbox-link-service.txt", "w+")
 
 from watchdog.observers import Observer
@@ -8,7 +9,7 @@ from watchdog.events import LoggingEventHandler
 from watchdog.events import PatternMatchingEventHandler  
 
 import logging
-import threading
+# import threading
 
 
 
@@ -25,12 +26,14 @@ def read_from_clipboard():
 
 class MyHandler(PatternMatchingEventHandler):
     def on_created(self, event):
-        print('handling new event!!!!!', flush=True)
-        ethread = threading.Thread(target=event_handler, args=(event,))
-        ethread.start()
-        #event_handler(event)
+        super(MyHandler, self).on_created(event)
 
-def event_handler(event):
+        print('handling new event!!!!!', flush=True)
+        # ethread = threading.Thread(target=thing_created, args=(event,))
+        # ethread.start()
+        thing_created(event)
+
+def thing_created(event):
     if any(x in event.src_path.lower() for x in ['screen shot', 'screenshot']):
         print('SCREEN SHOT MATCH!!!!!!!!!', event.src_path, flush=True)
     else:
@@ -43,28 +46,60 @@ def event_handler(event):
             print('clipboard has a dropbox url in it! swapping...', flush=True)
             clip = clip.replace('?dl=0', '?raw=1')
             write_to_clipboard(clip)
+            # print('exiting myself...', flush=True)
+            # observer.stop()
+            # os._exit(0)
+            # raise ExitCommand()
+            # exit(1)
             return
+
         else:
-            print('failed on', clip, flush=True)
+            print('failed to find ?dl=0 in clipboard..', clip, flush=True)
         time.sleep(0.3)
     return
     
 
 
-print('starting up!!!!!', flush=True)
+print('starting up!', flush=True)
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 path = "/Users/chris/Desktop"
-observer = Observer()
-#event_handler = LoggingEventHandler()
-#event_handler = MyHandler()
-observer.schedule(MyHandler(), path, recursive=False)
-observer.start()
+
+
+observer = None
+
+def setup_observer():
+    observer = Observer()
+    #event_handler = LoggingEventHandler()
+    #event_handler = MyHandler()
+    event_handler = PatternMatchingEventHandler(patterns=["*Screen Shot*.png"],
+                    ignore_patterns=[],
+                    ignore_directories=True)
+    event_handler.on_created = thing_created
+    # observer.schedule(MyHandler(), path, recursive=False)
+    observer.schedule(event_handler, path, recursive=False)
+    observer.daemon=False
+    observer.start()
+    return observer
+
 try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join()
+    observer_old = None
+    for x in range(0,600):
+        observer = setup_observer()
+        time.sleep(20)
+        observer.unschedule_all()
+        observer.stop()
+        observer.join()
+        del observer
+        print('restarting watcher...', flush=True)
+except:
+    import traceback
+    traceback.print_exc()
+finally:
+    print('exiting!')
+    if observer:
+	    observer.unschedule_all()
+	    observer.stop()
+    os._exit(1)
 
